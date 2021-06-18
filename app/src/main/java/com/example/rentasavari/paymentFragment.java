06850ab1b6fab223b3ee3c757a06c9b85484d637
sachin-detrax <84434145+sachin-detrax@users.noreply.github.com>
2021-06-18 22:18:37 +0530

    detail page added with payment

---
+++ app/src/main/java/com/example/rentasavari/paymentFragment.java
@@ -0,0 +1,36 @@
+package com.example.rentasavari;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class paymentFragment extends Fragment {
+
+
+
+
+    public paymentFragment() {
+
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_payment, container, false);
+        return view;
+    }
+}
