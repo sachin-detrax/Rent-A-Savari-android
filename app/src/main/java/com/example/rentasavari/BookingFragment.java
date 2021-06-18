9cf33125cdea513c45c91c75f794cd1a0fe0fec6
sachin-detrax <84434145+sachin-detrax@users.noreply.github.com>
2021-06-18 18:48:53 +0530

    added fragment of booking page backend still left

--- app/src/main/java/com/example/rentasavari/BookingFragment.java
+++ app/src/main/java/com/example/rentasavari/BookingFragment.java
@@ -3,6 +3,7 @@ package com.example.rentasavari;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +25,8 @@ public class BookingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_booking, container, false);
+        RecyclerView recyclerView;
+        recyclerView = view.findViewById(R.id.recyclerView);
         return view;
     }
 }
