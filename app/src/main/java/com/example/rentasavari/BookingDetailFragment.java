cdab26623dcf7da77a0b703999cf1514d3890218
sachin-detrax <84434145+sachin-detrax@users.noreply.github.com>
2021-06-18 19:36:35 +0530

    detail page added

---
+++ app/src/main/java/com/example/rentasavari/BookingDetailFragment.java
@@ -0,0 +1,31 @@
+package com.example.rentasavari;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BookingDetailFragment extends Fragment {
+
+    public BookingDetailFragment() {
+
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_booking_deatil, container, false);
+        return view;
+    }
+}
